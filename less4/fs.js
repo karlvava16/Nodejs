ce1fa072e360a94348beca482c7e65f31aae8118
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-09-10 14:27:09 +0300

    done task2

--- less4/fs.js
+++ less4/fs.js
@@ -1,4 +1,3 @@
-import { log } from 'node:console';
 import fs from 'node:fs';
 import path from 'node:path';
 
--- less4/index.js
+++ less4/index.js
@@ -1,19 +1,31 @@
 import { Logger } from './Logger.js';
+import path from 'node:path';
+import fs from 'node:fs';
 
 const __filename = import.meta.filename;
 
 const __dirname = import.meta.dirname;
 
-const logger = new Logger(__dirname);
+// const logger = new Logger(__dirname);
 
-logger.info('Done task async');
-logger.warn('File can be corrupted async');
-logger.error('File corrupted async');
+// logger.info('Done task async');
+// logger.warn('File can be corrupted async');
+// logger.error('File corrupted async');
 
-logger.infoSync('Done task sync');
-logger.warnSync('File can be corrupted sync');
-logger.errorSync('File corrupted sync');
+// logger.infoSync('Done task sync');
+// logger.warnSync('File can be corrupted sync');
+// logger.errorSync('File corrupted sync');
 
-console.log(logger.readLogSync());
+// console.log(logger.readLogSync());
 
-console.log(logger.readLog());
+// console.log(logger.readLog());
+
+//Task 2
+
+const data = fs.readFileSync(path.resolve(__dirname, 'logs.txt'), {
+    encoding: 'utf8',
+});
+
+const lines = data.split('\n').length;
+
+console.log(`Line amount: ${lines}`);
